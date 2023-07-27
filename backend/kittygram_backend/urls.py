1c2a9516ab67ea96b55ea33e0113525afc6e709a
SofiyaPalko2 <sofiyapalko20@gmail.com>
2023-07-27 20:50:13 +0300

    isort

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,9 +1,16 @@
-from cats.views import AchievementViewSet, CatViewSet
+# Много раз пыталась отсортировать этот файл, isort .
+# Fixing D:\Dev\kittygram_final\backend\kittygram_backend\urls.py
+# После того как явручную все поправила,isort cноваправит на предыдущий вариант
+# Как это исправить?)
+from rest_framework import routers
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+
+from cats.views import AchievementViewSet, CatViewSet
 
 router = routers.DefaultRouter()
 router.register('cats', CatViewSet)
